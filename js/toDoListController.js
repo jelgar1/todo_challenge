07778b9418aaa4db4bc1f5c0546360d8f8327284
Jelgar <jonathanelgardiner@gmail.com>
2016-01-30 12:42:34 +0000

    users can add tasks

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -2,4 +2,9 @@ toDoList.controller('ToDoListController', [function(){
     var self = this;
 
     this.taskList = [];
+
+    this.addTask = function(task){
+      this.taskList.push(task);
+      this.task =  null;
+    };
 }]);
--- test/todoListController.spec.js
+++ test/todoListController.spec.js
@@ -10,4 +10,9 @@ describe('ToDoListController', function() {
   it('initializes with an empty list', function(){
     expect(ctrl.taskList).toEqual([]);
   });
+
+  it('can add a task to the list', function() {
+    ctrl.addTask('Feed the cat');
+    expect(ctrl.taskList).toContain('Feed the cat');
+  });
 });
